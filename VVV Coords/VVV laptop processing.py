8852ae03936142864c03faaa986bb8ccdbf43988
Aaron <TheOnlyOrk@gmail.com>
2019-08-14 19:37:35 +0100

    Properly have 8000 VVV eclipsing files now

--- VVV Coords/VVV laptop processing.py
+++ VVV Coords/VVV laptop processing.py
@@ -11,6 +11,7 @@ types_to_process = ['cep','rrlyr','lpv','ecl1','ecl2']
 
 types_to_process = ['ecl1','ecl2']
 max_objects = 4000
+index = 0
 
 for file_type in types_to_process:
     if file_type in ['ecl1','ecl2']:
@@ -27,13 +28,15 @@ for file_type in types_to_process:
     obs_file = pd.read_csv(filename, header=11, dtype=column_type_dict)
     obs_file = obs_file.sort_values(['# sourceID','mjd'],ascending=True)
 
+    obs_file = obs_file.iloc[:800000]
+
     #Check if the processed files directory exists
     os.makedirs((base_path + '/VVV-' + trimed_file_type), exist_ok=True)
 
     previous_value_time = 0
     previous_value_mag = 0
     #last_line = a
-    index = 0
+    #index = 0 #Commenting this out since it was only giving 4000 eclipsing
     current_id = ''
     def batch_processor(a):
         global processed_objects
@@ -59,6 +62,9 @@ for file_type in types_to_process:
 
             previous_value_time = a['mjd']
             previous_value_mag = a['aperMag3']
+
+        if index > 3999:
+            print(index)
         return
 
     obs_file.apply(batch_processor, axis = 1)
